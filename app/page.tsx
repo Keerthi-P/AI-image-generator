a8312081abb2706efb4e202acf336f8fabf7a5f2
Keerthi-P <keerthi881992@gmail.com>
2024-09-07 17:29:28 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl">Hey everyone, This is Keerthi here</h1>
+    </div>
+  );
+}
